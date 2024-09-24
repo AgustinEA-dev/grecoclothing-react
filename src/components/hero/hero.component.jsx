4e951ecd7edaa9b0ac6d30922779dd72c276e1cd
unknown <ag_antro@hotmail.com>
2024-09-24 03:53:33 -0300

    last commit

--- src/components/hero/hero.component.jsx
+++ src/components/hero/hero.component.jsx
@@ -4,14 +4,11 @@ import "./hero.styles.scss";
 
 const Hero = () => {
   return (
-    <section className="hero-container">
       <div className="hero">
         <div className="hero-text-container">
           <h2 className="hero-h2">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
             iusto quam voluptate deleniti autem error soluta dolorem provident,
-            inventore cupiditate impedit perferendis eos laboriosam a tenetur
-            excepturi repellat animi molestiae?
           </h2>
           <Link className="hero-span" to={"/about"}>
             <span>Know more</span>
@@ -19,11 +16,13 @@ const Hero = () => {
         </div>
         <div className="hero-img-container">
           <div className="image-container">
-            <Link className="shop-link" to={"/products"}>SHOP</Link>
+            <Link className="shop-link" to={"/products"}>
+              SHOP
+            </Link>
           </div>
         </div>
       </div>
-    </section>
+
   );
 };
 
