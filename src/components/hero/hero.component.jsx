94ec2806baefd127f0482c4a88429e1d5d309a46
unknown <ag_antro@hotmail.com>
2024-09-24 14:39:03 -0300

    last commit

--- src/components/hero/hero.component.jsx
+++ src/components/hero/hero.component.jsx
@@ -6,18 +6,14 @@ const Hero = () => {
   return (
     <div className="hero">
       <div className="image-container">
-        <img className="hero-image" src="foto-hero.jpg" alt="" />
+        {/* <img className="hero-image" src="foto-hero.jpg" alt="" /> */}
         <Link className="shop-link" to={"/products"}>
           SHOP
         </Link>
       </div>
       <div className="hero-text-container">
-        <h2 className="hero-h2">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-          iusto quam voluptate deleniti autem error soluta dolorem provident,
-        </h2>
         <Link className="about-link" to={"/about"}>
-          <span>Know more</span>
+          <span>Know more about us</span>
         </Link>
       </div>
     </div>
