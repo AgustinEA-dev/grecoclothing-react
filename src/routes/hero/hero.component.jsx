41fccce1651bdf04672e937ae6a521d1482e9c5a
unknown <ag_antro@hotmail.com>
2024-10-09 12:50:46 -0300

    last commit

--- src/routes/hero/hero.component.jsx
+++ src/routes/hero/hero.component.jsx
@@ -10,6 +10,11 @@ const Hero = () => {
           <img className="logo" src="greco-white.png" alt="greco-white.png" />
         </Link>
       </div>
+      {/* <div className="shop-link-container">
+        <Link className="shop-link">
+          <span>SHOP</span>
+        </Link>
+      </div> */}
       <div className="hero-text-container">
         <Link className="about-link" to={"/about"}>
           <span>Know more about us</span>
